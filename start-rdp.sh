8fd84be71b50efa5ed9823a1f753cdf18f1f8d60
ilhamnih12 <hamyeu600@gmail.com>
2025-05-18 11:05:11 +0700

    Update start-rdp.sh

--- start-rdp.sh
+++ start-rdp.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 echo "[✔] Updating system and installing required packages..."
-sudo apt update && sudo apt install -y wget curl unzip sudo xvfb x11vnc fluxbox wine64 xrdp git openssh-client
+sudo apt update && sudo apt install -y wget curl unzip sudo xfce4 xfce4-goodies xrdp firefox git
 
 echo "[✔] Creating RDP user..."
 sudo useradd -m rdpuser || echo "User already exists"
 echo "rdpuser:rdppass" | sudo chpasswd
 sudo adduser rdpuser sudo
 
-echo "[✔] Starting X11 display..."
-Xvfb :1 -screen 0 1024x768x16 &
-export DISPLAY=:1
+echo "[✔] Configuring xrdp to use XFCE..."
+sudo sed -i.bak '/^test -x \/etc\/X11\/Xsession && exec \/etc\/X11\/Xsession/s/^/#/' /etc/xrdp/startwm.sh
+sudo sed -i '/^exec \/bin\/sh \/etc\/X11\/Xsession/s/^/#/' /etc/xrdp/startwm.sh
+echo "startxfce4" | sudo tee -a /etc/xrdp/startwm.sh
 
-echo "[✔] Starting lightweight desktop..."
-fluxbox &
-
-echo "[✔] Starting VNC server..."
-x11vnc -display :1 -nopw -forever &
-
-echo "[✔] Starting xrdp service..."
-sudo /etc/init.d/xrdp start
+echo "[✔] Restarting xrdp service..."
+sudo systemctl restart xrdp
 
 echo "[✔] Starting Pinggy tunnel..."
 ssh -o StrictHostKeyChecking=no -p 443 -R0:localhost:3389 tcp@free.pinggy.io
